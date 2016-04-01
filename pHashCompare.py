01220db4ac1835c4bf76758aa7accd8876fd7eb0
jhamiltonwork <jhamiltonwork@users.noreply.github.com>
2016-04-01 13:41:14 -0500

    Added files via upload

--- pHashCompare.py
+++ pHashCompare.py
@@ -1,81 +1,31 @@
-import csv
+import pandas as pd
+import time
 import hashlib
 import multiprocessing
 
 
-def ReadWrite(efile, filewrite, rfile):
-    with open(efile, 'rb') as csvfile:
-        with open(rfile, 'rb') as rhashfile:
-            with open(filewrite, 'w') as outputfile:
-                rnreader = csv.reader(rhashfile, delimiter=',', quotechar='|')
-                reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-                writer = csv.writer(outputfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                for row in (reader):
-                    rowstring = str(row)
-                    rowstring = '\n'.join(row)
-                    rowstring = rowstring.lower()
-                    rowstring = rowstring.strip()
-                    stringhash = Hashing(rowstring)
-                    sendrnlist = ReadRhash(rnreader)
-                    resToDict = makeDictionary(rowstring, stringhash)
-                    #                    cleanRlist = [x for x in sendrnlist if x is not None]
-                    #                    print cleanRlist
-                    cleanTuple = compareHash(resToDict, sendrnlist, writer)
-                    #                    print(cleanTuple)
-
-    csvfile.close()
-    rhashfile.close()
-    outputfile.close()
+def ReadCSV(pemail, rhash, output):
+    coln = ['Email']
+    coln2 = ['Hash']
+    pcsv = pd.read_csv(pemail, names=coln)
+    rcsv = pd.read_csv(rhash, names=coln2)
+    pcsv['Email'] = pcsv['Email'].map(lambda x: x.lower().strip())
+    pcsv['Hash'] = pcsv['Email'].map(lambda s: hashlib.sha256(s).hexdigest().upper().strip())
+    common = pcsv.merge(rcsv, on=['Hash'])
+    results = ['Email']
+    common.to_csv(output, sep=',', columns=results, header=False, index=False, encoding='utf-8')
 
 
-def Hashing(lines):
-    for line in lines:
-        hashes = hashlib.sha256(lines.encode('utf-8')).hexdigest()
-        hashes = hashes.upper()
-        #        hashSplit = ','.join(hashes)
-        return hashes
+def WriteCSV(filewrite):
+    pass
 
 
-def ReadRhash(rnhashlist):
-    for liners in rnhashlist:
-        #        listers = tuple(liners)
-        return liners
-
-
-def makeDictionary(emailp, hashp):
-    rdictionary = {}
-    rdictionary[emailp] = hashp
-    return rdictionary
-
-
-def compareHash(pDict, rhashList, filewriter):
-    cleanedNoneTypeList = []
-    if rhashList:
-        cleanedNoneTypeList = cleanList(rhashList)
-    else:
-        pass
-    cset = set(cleanedNoneTypeList)
-    for key, value in pDict.items():
-        if cset and value[0] in cset:
-            filewriter.writerow([key])
-
-
-def cleanList(receiveList):
-    for lines in receiveList:
-        if receiveList:
-            return lines
-        break
-
-
-
-emailList = ('EmailTest.csv')
-output = ('pHashComp2Emailresult.csv')
-rhashlist = ('rHashed.csv')
-# rowsInFile = ReadWrite(emailList, output, rhashlist)
-# rnHashList = ReadRHash(rhashlist)
-
-
+pemailcsv = ('pEmails.csv')
+rhashcsv = ('RNhashReturn.csv')
+outfile = ('pHashToEmail.csv')
 # ------------ MAIN SCRIPT STARTS HERE -----------------
 if __name__ == '__main__':
-    emailsProcessed = multiprocessing.Process(target=ReadWrite(emailList, output, rhashlist))
-    emailsProcessed.start()
+    startTime = time.time()
+    results = ReadCSV(pemailcsv, rhashcsv, outfile)
+    elapsedTime = time.time() - startTime
+    print 'Duration: ', elapsedTime
